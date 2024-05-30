94e585b6a5ea0315d394320fb7e805a7c7d6a937
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-30 16:40:59 +0800

    switch case

--- CQ.cpp
+++ CQ.cpp
@@ -119,6 +119,31 @@ int main()
             cout << "3. Display Values " << endl;
             cout << "4. Exit" << endl;
             cout << "Enter your Choice :" << endl;
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+                case '1':
+                {
+                    q.insert();
+                    break;
+                }
+                case '2':
+                {
+                    q.remove();
+                    break;
+                }
+                case '3':
+                {
+                    q.display();
+                    break;
+                }
+                case '4':
+                {
+                    return 0;
+                }
+            }
         }
     }
 }

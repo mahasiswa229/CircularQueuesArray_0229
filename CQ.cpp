4ff730f1c62862c3cf0580b17ce9367454a6bebf
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-30 16:06:35 +0800

    pt2

--- CQ.cpp
+++ CQ.cpp
@@ -50,7 +50,20 @@ class Queues
            return;
         }
         cout << "\nThe elments deleted from thr queue is : " << queue_array[front] << "\n"
+        if (front == rear)
+    {
+        front = -1;
+        rear = -1;
     }
+    else
+    {
+        if(front == max - 1)
+        front = 0;
+        else
+        front = front + 1; 
+    }
+}
+};
 
     
 

15b948ecd9e1d491fe5735d6b54c6e0da8c3b232
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-30 16:37:10 +0800

    option

--- CQ.cpp
+++ CQ.cpp
@@ -109,7 +109,18 @@ int main()
     Queues q;
     char ch;
 
-    
+    while(true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display Values " << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your Choice :" << endl;
+        }
+    }
 }
 
     

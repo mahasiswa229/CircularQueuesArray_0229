542d1f01383bbb080395c0b226131fcf2203b78c
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-30 15:53:06 +0800

    jikalau

--- CQ.cpp
+++ CQ.cpp
@@ -21,6 +21,25 @@ class Queues
         cout << "Enter a Number : ";
         cin >> num;
         cout << endl;
+
+        if ((front == 0 && rear == max - 1) || (front == rear + 1 ))
+    {
+        cout << "\nQueue overflow\n";
+        return;
     }
+    if (front == -1)
+    {
+        front = 0;
+        rear =0;
+    }
+    else
+    {
+        if(rear == max - 1)
+        rear = 0;
+        else
+        rear = rear + 1;
+    }
+    queue_array[rear] = num;
+    }   
 };
 
